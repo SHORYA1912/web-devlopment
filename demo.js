eac1fd2f152a10d81e1f0f8de68f6e287e0d9178
SHORYA1912 <shoryarp77@gmail.com>
2025-03-04 20:50:01 +0530

    Add files via upload

--- demo.js
+++ demo.js
@@ -1,1 +1,4 @@
-console.log("welcome to web devlpoment");
+let name = "Sumit";
+let favouritesubject = "Maths";
+let grade = 5;
+console.log(`My name is ${name}.My faourite subject is ${favouritesubject}.My grade is ${grade}.`)
