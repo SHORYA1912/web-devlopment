74df7d550120ca4b192981e49f5081d5ce0538b4
SHORYA1912 <shoryarp77@gmail.com>
2025-04-30 17:31:51 +0530

    Add files via upload

---
+++ rectangle.py
@@ -0,0 +1,19 @@
+import turtle
+
+turtle.Screen().bgcolor("palegreen")
+
+board = turtle.Turtle()
+
+sc =turtle.Screen()
+sc.setup(500,500)
+
+board.forward(150)
+board.right(90)
+
+board.forward(75)
+board.right(90)
+
+board.forward(150)
+board.right(90)
+
+board.forward(75)
