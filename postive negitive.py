adee0cedef5f6fbc5459755ebcc6745d4749b184
SHORYA1912 <shoryarp77@gmail.com>
2025-04-30 19:23:23 +0530

    Add files via upload

---
+++ postive negitive.py
@@ -0,0 +1,11 @@
+number = int(input("ENTER THE NUMBER TO CHECK"))
+print("THE NUMBER TO BE CHECKED:",number)
+
+if number > 0:
+ print ("THE NUMBER IS POSITIVE")
+
+elif number < 0:
+ print("THE NUMBERIS NEGITVE")
+
+else :
+ print("THIS NUMBER IS NEUTRAL")
