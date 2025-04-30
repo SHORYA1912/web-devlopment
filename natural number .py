b4dca5d1b30e47bec2c7dae48254dbf16b6dcdc0
SHORYA1912 <shoryarp77@gmail.com>
2025-04-30 19:51:06 +0530

    Add files via upload

---
+++ natural number .py
@@ -0,0 +1,8 @@
+number = int(input("THE NUMBER TO BE CHECKED"))
+print("THE NUMBER TO BE CHECKED:",number)
+
+if number>0:
+    print("the number is Natural number")
+
+else:
+    print("the number is not Natural number")
