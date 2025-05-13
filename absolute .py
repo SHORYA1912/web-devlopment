7e29ab99d2f90a1625b7e46df58823ae03fb5731
SHORYA1912 <shoryarp77@gmail.com>
2025-05-13 20:27:27 +0530

    Add files via upload

---
+++ absolute .py
@@ -0,0 +1,9 @@
+def absolute(number):
+
+    if number>=0:
+        return number
+    else:
+        return -number
+    
+print("the absolute value of 89 =",absolute(89))
+print("the absloute value of -189 =",absolute(-189))
