2a7c98e32bc67c59b0df79d1a54de15158ebc75f
SHORYA1912 <shoryarp77@gmail.com>
2025-08-05 20:43:16 +0530

    Add files via upload

---
+++ ai bot.py
@@ -0,0 +1,14 @@
+print("I AM A AI BOT")
+print("WHAT IS YOUR NAME?")
+name = input()
+print("NICE T MEET YOU"+ name +"!")
+print("HOW ARE YOU FEELING TODAY?:(GOOD/BAD)")
+mood = input().lower()
+if mood == "good":
+    print("I AM GLAD TO HEAR THAT!")
+elif mood == "bad":
+    print("I AM SORRY TO HEAR THAT, I HOPE YOUR DAYS GET BETTER!")
+else:
+    print("I SEE ,SOMETIMES IT IS HARD TO PUT FEELING INTO WORDS!")
+
+print("I HAD A NICE CONVERSATION WITH YOU,"+name+"!" )
