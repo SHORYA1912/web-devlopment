627220e379fff3392ede7f1454b06711c7e30dff
SHORYA1912 <shoryarp77@gmail.com>
2025-09-03 22:32:04 +0530

    Add files via upload

--- AIEPCM2L2A2.py
+++ AIEPCM2L2A2.py
@@ -1,10 +1,8 @@
-# Rotating and Adjusting Brightness
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 image = cv2.imread('example.jpg')
 image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-# Rotate the image by 45 degrees around its center
 (h, w) = image.shape[:2]
 center = (w//2, h//2)
 M = cv2.getRotationMatrix2D(center, 45, 1.0) # rotate by 45 degrees
@@ -13,8 +11,6 @@ rotated_rgb = cv2.cvtColor(rotated, cv2.COLOR_BGR2RGB)
 plt.imshow(rotated_rgb)
 plt.title("Rotated Image")
 plt.show()
-# Increase brightness by adding 50 to all pixel values
-# Use cv2.add to avoid negative values or overflow
 brightness_matrix = np.ones(image.shape, dtype="uint8") * 50
 brighter = cv2.add(image, brightness_matrix)
 brighter_rgb = cv2.cvtColor(brighter, cv2.COLOR_BGR2RGB)
