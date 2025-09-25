cd22ed3d2c4084f279ebdbc827bddd0231520c12
SHORYA1912 <shoryarp77@gmail.com>
2025-09-25 22:30:51 +0530

    Add files via upload

--- filter.py
+++ filter.py
@@ -9,6 +9,12 @@ def apply_filter(img,mode):
         filtered[:,:, [0,2]] = 0
     if mode == "blue_tint":
         filtered[:,:,1:3] = 0
+    if mode == "yellow_tint":
+        filtered[:,:,0] = 0
+    if mode == "purple_tint":
+        filtered[:,:,1] = 0
+    if mode == "cyan_tint":
+        filtered[:,:,2] = 0
     if mode == "increased_red":
         filtered[:,:,2] = np.clip(filtered[:,:,2]+ 100,0,255)
     if mode == "decreased_blue":
@@ -21,14 +27,17 @@ if img is None:
      exit()
 
 filters = {
-    ord ("r"): "red_tint",
-    ord ("g"): "green_tint",
-    ord ("b"): "blue_tint",
-    ord ("i"): "increased_red",
-    ord ("d"): "decreased_blue",
-    ord ("o"): "original"
+    ord("r"): "red_tint",
+    ord("g"): "green_tint",
+    ord("b"): "blue_tint",
+    ord("i"): "increased_red",
+    ord("d"): "decreased_blue",
+    ord("y"): "yellow_tint",
+    ord("p"): "purple_tint",
+    ord("c"): "cyan_tint",
+    ord("o"): "original"
 }
-print("PRESS: r,g,b,i,d,o to apply filters, q to quit")
+print("PRESS: r=red, g=green, b=blue, i=increased red, d=decreased blue, y=yellow, p=purple, c=cyan, o=original, q=quit")
 while True:
     cv2.imshow("Image", img)
     key = cv2.waitKey(0) & 0xFF
