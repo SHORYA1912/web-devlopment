fc5e201a03cc668e2590008a334d1bcf02682c25
SHORYA1912 <shoryarp77@gmail.com>
2025-09-30 20:21:29 +0530

    Add files via upload

---
+++ FACE DETECTION.py
@@ -0,0 +1,27 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("ERROR: Cannot open camera")
+    exit()
+
+while True:
+    ret ,frame = cap.read()
+    if not ret:
+        print("ERROR: Cannot receive frame")
+        break
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+    cv2.imshow('Face Detection: PRESS Q TO QUIT', frame) 
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
