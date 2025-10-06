e990ba60a5fa2d3a6d3c185a9b65bbb9834a2904
SHORYA1912 <shoryarp77@gmail.com>
2025-10-06 22:40:56 +0530

    Add files via upload

--- FACE DETECTION.py
+++ FACE DETECTION.py
@@ -9,17 +9,13 @@ if not cap.isOpened():
     exit()
 
 while True:
-    ret ,frame = cap.read()
+    ret, frame = cap.read()
     if not ret:
         print("ERROR: Cannot receive frame")
         break
+
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-
-    cv2.imshow('Face Detection: PRESS Q TO QUIT', frame) 
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
