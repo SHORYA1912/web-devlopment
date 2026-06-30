2d019e1eadbe1c76b60d71dda799694a3f162c0c
SHORYA1912 <shoryarp77@gmail.com>
2026-06-30 20:29:47 +0530

    Add files via upload

---
+++ cleaning.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("country_vaccinations.csv")
+
+df.head(10)
+
+df.isnull().any()
+
+subset = df.iloc[:5200, :]
+plt.figure(figsize=(12 , 8))
+sns.heatmap(subset.isnull() ,cbar = False, cmap = "viridis")
+plt.show()
+
+df.head(10)
+
+df.dropna(how = all)
+
+df.filling(method = "BFILL")
+
+df.interpolate()
+
+df_dropped = df.dropna()
+df_dropped
